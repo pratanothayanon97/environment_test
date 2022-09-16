8b80c3a5a2a460ca2fe1ea5b4070c7fc11daa2db
root <root@localhost>
2022-09-16 00:22:29 +0000

    Book Assignment

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'JKRowling', price: 20, published: '2008-12-04')
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +12,21 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a published date' do
+    subject.published = nil
     expect(subject).not_to be_valid
   end
 end
